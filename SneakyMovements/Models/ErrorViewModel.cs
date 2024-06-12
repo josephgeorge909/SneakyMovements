b36d4b1aef5d8fc412f9bd8edea699d18e334c18
Joseph George <jgeorge2177@gmail.com>
2024-06-12 15:39:06 +0100

    MVC Web App Refactor

---
+++ SneakyMovements/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace SneakyMovements.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
